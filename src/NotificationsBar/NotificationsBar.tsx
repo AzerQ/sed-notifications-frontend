ec418cd85e3d2c30e02dd93e566705cff9cbed5f
AzerQ <46249148+AzerQ@users.noreply.github.com>
2025-10-08 05:16:20 +0000

    fix(NotificationsBar): reset default notification status to an empty string

--- src/NotificationsBar/NotificationsBar.tsx
+++ src/NotificationsBar/NotificationsBar.tsx
@@ -35,7 +35,7 @@ const NotificationsBarContent: React.FC<{
   const [filters, setFilters] = useState<Filters>({
     type: '',
     subtype: '',
-    status: 'unread',
+    status: '',
     starred: '',
     author: ''
   });
